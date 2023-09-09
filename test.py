326d99e52faf3d9c47ba46e738cb46b34dfcf558
Cleber Mota <clebermota@souunisuam.com.br>
2023-09-08 23:23:32 -0300

    Operação matemática

---
+++ test.py
@@ -0,0 +1,15 @@
+#Esse é um código
+
+ops = int(input("Escolha a operação: "))
+print("1 - soma")
+print("2 - subtração")
+print("3 - multiplicação")
+print("4 - divisão")
+print()
+num1 = int(input("Insira um numero: "))
+print() 
+num2 = int(input("Insira outro numero: ")) 
+if ops == soma:
+    print(num1 + num2)
+    if ops == s5ubtração:
+        print(num1 - num2)
